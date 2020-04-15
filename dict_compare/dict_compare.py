2ae4c8a6b04550e8d0ee8434073104e23ab1dad1
HowellWang <wangyh0827@gmail.com>
2020-04-14 21:40:19 -0700

    initial commit

---
+++ dict_compare/dict_compare.py
@@ -0,0 +1,46 @@
+from collections import Counter
+
+
+def is_same_dict(dict1, dict2):
+    """ Recursively compare the two dictionaries.
+    :param dict1: First dictionary
+    :param dict2: Second dictionary
+    :return: True if the dictionaries match
+    """
+
+    def is_same_list(l1, l2):
+        is_match = False
+        try:
+            if Counter(l1) == Counter(l2):
+                return True
+        except TypeError:
+            pass
+        if len(l1) == len(l2) and len(l1) > 0:
+            for v1e, v2e in zip(l1, l2):
+                is_match = v1e == v2e
+                if not is_match:
+                    if isinstance(v1e, dict) and isinstance(v2e, dict):
+                        is_match = is_same_dict(v1e, v2e)
+                    elif isinstance(v1e, list) and isinstance(v2e, list):
+                        is_match = is_same_list(v1e, v2e)
+                if not is_match:
+                    return is_match
+        return is_match
+
+    if dict1 == dict2:
+        return True
+
+    if set(dict1.keys()) != set(dict2.keys()):
+        return False
+    for k, v1 in sorted(dict1.items()):
+        v2 = dict2[k]
+        if v1 != v2:
+            if isinstance(v1, dict) and isinstance(v2, dict):
+                if not is_same_dict(v1, v2):
+                    return False
+            elif isinstance(v1, list) and isinstance(v2, list):
+                if not is_same_list(v1, v2):
+                    return False
+            else:
+                return False
+    return True
---
+++ test/test_dict_compare.py
@@ -0,0 +1,41 @@
+import unittest
+
+from dict_compare.dict_compare import *
+
+
+class DictCompareTestCase(unittest.TestCase):
+    def test_basic(self):
+        d1 = dict(a='foo')
+        d2 = dict(a='foo')
+        self.assertTrue(is_same_dict(d1, d2))
+        self.assertFalse(is_same_dict(d1, dict(a='bar')))
+
+    def test_dict_with_list_value(self):
+        d1 = dict(a=[1, 2])
+        d2 = dict(a=[2, 1])
+        try:
+            self.assertEqual(d1, d2)
+        except AssertionError:
+            self.assertTrue(is_same_dict(d1, d2))
+
+    def test_add(self):
+        d1 = dict(a='foo', b=1, c=[1, 2, 3])
+        self.assertFalse(is_same_dict(d1, dict()))
+
+    def test_depth(self):
+        d1 = dict(a=dict(b=dict(c="c1", d="d1")))
+        self.assertFalse(is_same_dict(d1, dict(a=dict(b=dict(c="c1")))))
+
+    def test_compare_dicts(self):
+        d1 = dict(a=['andy', 'john', dict(rel='brothers')])
+        d2 = dict(a=['john', 'andy', dict(rel='brothers')])
+        self.assertFalse(is_same_dict(d1, d2))
+
+    def test_different_lengths(self):
+        d1 = dict(a=["a", "b", dict(c='foo')])
+        d2 = dict(a=["a", "b"])
+        self.assertFalse(is_same_dict(d1, d2))
+
+
+if __name__ == '__main__':
+    unittest.main()
